7d518185b8b76113ee026592173ca9df10cc5f1b
Samridh Amla <samridhamla@microsoft.com>
2021-10-14 15:53:04 +0530

    init

--- main.py
+++ main.py
@@ -7,28 +7,26 @@ Created on Wed Oct 13 17:11:14 2021
 from flask import Flask, request, jsonify
 from liveTraffic import *
 from flask_cors import CORS, cross_origin
+import json
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route("/hello")
+@app.route("/api")
 @cross_origin()
 def index():
     print("Brooo")
-    #myUrl =  request.args.get("url")
-    #myUrl = "https://www.youtube.com/watch?v=" + myUrl;
-    myUrl = "vDHtypVwbHQ"
-    #output = processVideo(myUrl) 
+    myUrl =  request.args.get("url")
+    myUrl = "https://www.youtube.com/watch?v=" + str(myUrl);
+    #myUrl = "vDHtypVwbHQ"
+    output = processVideo(myUrl) 
     #output["url"]  = myUrl
-    mydict = {}
-    mydict["1"] = "car"
-    mydict["2"] = "person"
-    response = jsonify(mydict)
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    return response
+    #response.headers.add("Access-Control-Allow-Origin", "*")
+    json_string = json.dumps(output)
+    print(json_string)
+    return json_string
 
 
 if __name__ == "__main__":
-    #app.run(host="127.0.0.1", port=8080, debug=True)
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="127.0.0.1", port=8080, debug=True)
